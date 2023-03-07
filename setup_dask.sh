06cd5be81004ec273e8858314c0401e29c94ff72
Matt Lane <lane.matthew.joseph@gmail.com>
2023-03-07 10:41:09 -0500

    setup dask

---
+++ setup_dask.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+module load ums
+module load ums-gen119
+module load nvidia-rapids
+
+SCHEDULER_DIR=/gpfs/alpine/syb105/proj-shared/Personal/lanemj/projects/climatypes/dask_neighbors/dask_scheduler
+WORKER_DIR=/gpfs/alpine/syb105/proj-shared/Personal/lanemj/projects/climatypes/dask_neighbors/worker_dir
+
+SCHEDULER_FILE=$SCHEDULER_DIR/my-scheduler.json
+
+echo 'Running scheduler'
+jsrun --nrs 1 --tasks_per_rs 1 --cpu_per_rs 2 --smpiargs="-disable_gpu_hooks" \
+      dask-scheduler --interface ib0 --scheduler-file $SCHEDULER_FILE \
+                     --no-dashboard --no-show &
+
+#Wait for the dask-scheduler to start
+sleep 10
+
+jsrun --rs_per_host 6 --tasks_per_rs 1 --cpu_per_rs 2 --gpu_per_rs 1 --smpiargs="-disable_gpu_hooks" \
+      dask-cuda-worker --nthreads 1 --memory-limit 82GB --device-memory-limit 16GB --rmm-pool-size=15GB \
+                       --interface ib0 --scheduler-file $SCHEDULER_FILE --local-directory $WORKER_DIR \
+                       --no-dashboard &
+
+#Wait for WORKERS
+sleep 10
+
+WORKERS=12
+
+#python -u $CONDA_PREFIX/examples/blazingsql/bsql_test_multi.py $SCHEDULER_FILE #WORKERS
+
+#wait
+
+#clean LOG files
+#rm -fr $SCHEDULER_DIR
